86ea77c76b4258ad03cbb9cd4aab082099b4b9d7
Franz Becker <becker.franz@gmail.com>
2015-09-04 16:09:01 +0200

    add some debug-echo for publish script

--- .travis-publishOnRelease.sh
+++ .travis-publishOnRelease.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Execute only if branch is 'master' and a tag exists that starts with 'v'
+echo "TRAVIS_BRANCH = $TRAVIS_BRANCH"
+echo "TRAVIS_TAG = $TRAVIS_TAG"
+
 if [[ $TRAVIS_BRANCH == "master" && -n "$TRAVIS_TAG" && "$TRAVIS_TAG" == v* ]]; then
     echo "Publishing version: $TRAVIS_TAG"
     echo "gradle.publish.key=$GRADLE_PUBLISH_KEY" >> gradle.properties
