2a9d1076fec0f8e6e586c21dc37c85458e57b8b6
Franz Becker <becker.franz@gmail.com>
2015-09-04 19:54:13 +0200

    fix publish script

--- .travis-publishOnRelease.sh
+++ .travis-publishOnRelease.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# Execute only if branch is 'master' and a tag exists that starts with 'v'
-echo "TRAVIS_BRANCH = $TRAVIS_BRANCH"
-echo "TRAVIS_TAG = $TRAVIS_TAG"
+# Execute only on tag builds where the tag starts with 'v'
 
-if [[ $TRAVIS_BRANCH == "master" && -n "$TRAVIS_TAG" && "$TRAVIS_TAG" == v* ]]; then
+if [[ -n "$TRAVIS_TAG" && "$TRAVIS_TAG" == v* ]]; then
     echo "Publishing version: $TRAVIS_TAG"
     echo "gradle.publish.key=$GRADLE_PUBLISH_KEY" >> gradle.properties
     echo "gradle.publish.secret=$GRADLE_PUBLISH_SECRET" >> gradle.properties
