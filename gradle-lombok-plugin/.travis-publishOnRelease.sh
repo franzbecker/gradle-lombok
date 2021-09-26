2d9e0039bab096925b0be92583e73e57fb64f18b
Franz Becker <becker.franz@gmail.com>
2021-09-26 19:01:04 +0100

    remove bintray code

--- gradle-lombok-plugin/.travis-publishOnRelease.sh
+++ gradle-lombok-plugin/.travis-publishOnRelease.sh
@@ -5,6 +5,5 @@ if [[ -n "$TRAVIS_TAG" && "$TRAVIS_TAG" == v* ]]; then
     echo "Publishing version: $TRAVIS_TAG"
     echo "gradle.publish.key=$GRADLE_PUBLISH_KEY" >> gradle.properties
     echo "gradle.publish.secret=$GRADLE_PUBLISH_SECRET" >> gradle.properties
-    ./gradlew bintrayUpload
     ./gradlew publishPlugins
 fi
